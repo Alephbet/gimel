c914512e35795b22611b365d0846a9652805bc83
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2016-05-10 09:12:51 +0200

    Update packaging manifest

--- setup.py
+++ setup.py
@@ -3,6 +3,18 @@
 from setuptools import setup, find_packages
 import os
 
+try:
+    from pypandoc import convert
+
+    def read_md(readme_file):
+        return convert(readme_file, 'rst')
+except ImportError:
+    print("warning: pypandoc module not found, could not convert Markdown to RST")
+
+    def read_md(readme_file):
+        with open(readme_file, 'r') as f:
+            return f.read()
+
 requires = [
     'awscli>=1.10.21',
     'jmespath>=0.9.0',
@@ -11,12 +23,11 @@ requires = [
     'redis>=2.10.5'
 ]
 
-
 setup(
     name='gimel',
     version=open(os.path.join('gimel', 'VERSION')).read().strip(),
     description='Run your own A/B testing backend on AWS Lambda',
-    long_description=open('README.md').read(),
+    long_description=read_md('README.md'),
     author='Yoav Aner',
     author_email='yoav@gingerlime.com',
     url='https://github.com/Alephbet/gimel',
