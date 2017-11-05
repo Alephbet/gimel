e092aead60311f25de3d0a07257e85a57f2c0851
Yoav <yoav@gingerlime.com>
2017-11-05 16:01:23 +0100

    marked as production/stable (have been running in production successfully for a while)

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     install_requires=requires,
     license=open("LICENSE").read(),
     classifiers=(
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Intended Audience :: System Administrators',
         'Natural Language :: English',
