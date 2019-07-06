51b4cac19bd31e14ca13b6b5a3f63d6d587c953d
Yoav <yoav@gingerlime.com>
2019-07-06 20:01:58 +0200

    fixing setup to use markdown

--- setup.py
+++ setup.py
@@ -3,17 +3,8 @@
 from setuptools import setup, find_packages
 import os
 
-try:
-    from pypandoc import convert
-
-    def read_md(readme_file):
-        return convert(readme_file, 'rst')
-except ImportError:
-    print("warning: pypandoc module not found, could not convert Markdown to RST")
-
-    def read_md(readme_file):
-        with open(readme_file, 'r') as f:
-            return f.read()
+with open('README.md') as f:
+    long_description = f.read()
 
 requires = [
     'awscli>=1.10.21',
@@ -27,7 +18,8 @@ setup(
     name='gimel',
     version=open(os.path.join('gimel', 'VERSION')).read().strip(),
     description='Run your own A/B testing backend on AWS Lambda',
-    long_description=read_md('README.md'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Yoav Aner',
     author_email='yoav@gingerlime.com',
     url='https://github.com/Alephbet/gimel',
@@ -39,7 +31,6 @@ setup(
         gimel=gimel.cli:cli
     """,
     install_requires=requires,
-    license=open("LICENSE").read(),
     classifiers=(
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
